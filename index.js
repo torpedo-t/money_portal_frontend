b5462387d19974d7e963fd69cf1b491698343b60
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2022-02-03 10:40:05 -0600

    get request implementation

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("in index.js");
+const endPoint = "http://localhost:3000/api/v1/bank_accounts"
+
+document.addEventListener('DOMContentLoaded', () => {
+    getBankAccounts()
+    })
+
+    function getBankAccounts() {
+        fetch(endPoint)
+        .then(response => response.json())
+        .then(accounts => {
+            accounts.data.forEach(bankAccount => {
+                debugger
+                const bankAccountMarkup = `
+                `
+            })
+    })
+}
